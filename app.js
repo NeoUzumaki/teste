1340a70e93e2bf15df1cdc5127c5909ee7dda889
José Jorge silva <josejorgesilvaneto@gmail.com>
2025-01-21 19:01:45 -0300

    terceiro dan

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Insira um número de 1 a 10';
+
+function verificarChute() {
+    console.log('Funcionou');
+};
